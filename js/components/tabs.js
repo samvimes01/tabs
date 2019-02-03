38c6adabfc9babd47ecbae5c487b0f468822294a
SamVimes0 <k.aleksandr88@gmail.com>
2019-02-03 14:35:46 +0200

    first commit

---
+++ js/components/tabs.js
@@ -0,0 +1,13 @@
+export default class Tabs {
+  constructor({ element }) {
+    this._element = element;
+
+    this._render();
+  }
+
+  _render() {
+    this._element.innerHTML = `
+      
+    `;
+  }
+}
